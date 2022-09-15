230b72288d2ecbaa052ed343e1bd1ce8db30f39c
prof <richardalimahowa@gmail.com>
2022-09-15 12:36:17 -0700

    javasript

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,60 @@
+
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+var job = 'programmer';
+job = 'teacher';
+
+//lastName = 'Schmedtmann';
+//console.log('Schmedtmann');
+// math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Dog p';
+console.log(firstName +  ' ' + lastName);
+// assignment operators
+let x = 10 + 5;
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+console.log(x);
+
+// comparison operators
+console.log(ageJonas > ageSarah); //  >, <, >=, <=
+console.log(ageSarah >= 18);
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2020);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
